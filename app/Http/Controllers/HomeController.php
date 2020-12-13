abf9d32dd1b645129d7633366a742f140e9657f3
Luan Oliveira <luanoliveira17@hotmail.com>
2020-12-13 19:43:03 -0300

    Criando controller e view de home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Usuario;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return "HOME";
+        $quantidades = (object) array(
+            "usuarios" => Usuario::count()
+        );
+        // dd($quantidades->usuarios);
+        return view('admin.dashboard.home', compact('quantidades'));
     }
 }
---
+++ resources/views/admin/dashboard/home.blade.php
@@ -0,0 +1,7 @@
+<x-layouts.admin>
+    <x-slot name="title">
+        Teste
+    </x-slot> 
+    <h1>QUANTIDADES</h1>
+    <div><b>Usuários: </b>{{$quantidades->usuarios}}</div>
+</x-layouts.admin>
