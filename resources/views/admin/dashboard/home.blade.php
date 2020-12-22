3dbb372e6f35e7d52100b808ed1fd10f702bba65
Luan Oliveira <luanoliveira17@hotmail.com>
2020-12-22 16:12:58 -0300

    Criando box component

--- resources/views/admin/dashboard/home.blade.php
+++ resources/views/admin/dashboard/home.blade.php
@@ -1,7 +1,5 @@
-<x-layouts.admin>
-    <x-slot name="title">
-        Teste
-    </x-slot> 
+<x-layouts.admin title="Dashboard" currentPage="home">
+    <x-dashboard.box/>
     <h1>QUANTIDADES</h1>
     <div><b>Usuários: </b>{{$quantidades->usuarios}}</div>
 </x-layouts.admin>
---
+++ resources/views/components/dashboard/box.blade.php
@@ -0,0 +1,17 @@
+<div class="col-lg-2 col-sm-6">
+    <div class="circle-tile">
+        <a href="#">
+            <div class="circle-tile-heading dark-blue">
+                <i class="fa fa-users fa-fw fa-3x"></i>
+            </div>
+        </a>
+        <div class="circle-tile-content dark-blue">
+            <div class="circle-tile-description text-faded">Usuários</div>
+            <div class="circle-tile-number text-faded">265</div>
+        </div>
+        <a href="#" class="circle-tile-footer">
+            "Mais Informações"
+            <i class="fa fa-chevron-circle-right"></i>
+        </a>
+    </div>
+</div>
