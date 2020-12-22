a2dd81f3d15bcf932d9c34887926e5f1c543f880
Luan Oliveira <luanoliveira17@hotmail.com>
2020-12-22 18:08:51 -0300

    Component de footer

---
+++ resources/views/components/layout/parts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2020 <a href="#">Luan Oliveira</a>.</strong>
+    Todos direitos reservados.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 0.1
+    </div>
+</footer>
