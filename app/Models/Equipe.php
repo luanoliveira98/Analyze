2c77ea3a36647381c3f57e982c8455d4d31d66bd
Luan Oliveira <luanoliveira17@hotmail.com>
2020-12-28 12:03:02 -0300

    Criando scope de adversarios

--- app/Models/Equipe.php
+++ app/Models/Equipe.php
@@ -10,8 +10,19 @@ class Equipe extends Model
     use softDeletes;
     protected $table = 'equipes';
 
+    /**
+     * Lista as line ups da organização
+     */
     public function scopeLines($query)
     {
         return $query->where('b_adversario', 0);
+    }
+
+    /**
+     * Lista os adversários
+     */
+    public function scopeAdversarios($query)
+    {
+        return $query->where('b_adversario', 1);
     }
 }
