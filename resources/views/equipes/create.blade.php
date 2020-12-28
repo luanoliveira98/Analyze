47eaa295aaa221cb0b2ef0009bb94d5410b19c12
Luan Oliveira <luanoliveira17@hotmail.com>
2020-12-28 02:01:22 -0300

    Criando view de inserir equipe

---
+++ resources/views/equipes/create.blade.php
@@ -0,0 +1,14 @@
+<x-layouts.admin title="Inserir Equipe" active="equipes" :breadcrumbs="$breadcrumbs">
+    <x-cards.base>
+        <x-cards.header title="Inserir Equipe">
+        </x-cards.header>
+        <x-cards.body>
+            <x-forms.base action="{{route('admin.equipes.salvar')}}" method="post">
+                <x-forms.input name="nome" label="Nome" placeholder="Nome da Equipe"/>
+                <div class="text-right">
+                    <button type="submit" class="btn btn-primary">Salvar</button>
+                </div>
+            </x-forms.base>
+        </x-cards.body>
+    </x-cards.base>
+</x-layouts.admin>
