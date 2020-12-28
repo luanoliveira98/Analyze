5cc6594040d472774f2aa19c3d9142dedfb29944
Luan Oliveira <luanoliveira17@hotmail.com>
2020-12-28 02:01:35 -0300

    Criando rota para salvar equipe

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,20 @@
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/admin')->name('admin.')->group(function () {
-    Route::get('/', 'HomeController@index')->name('home');
-    Route::get('/logout', 'HomeController@index')->name('logout');
+    Route::get('/',         'HomeController@index')->name('home');
+    Route::get('/logout',   'HomeController@index')->name('logout');
 
     Route::name('membros.')->prefix('/membros')->group(function () {
-        Route::get('/administracao', 'HomeController@index')->name('administracao');
-        Route::get('/staff', 'HomeController@index')->name('staff');
-        Route::get('/elenco', 'HomeController@index')->name('elenco');
+        Route::get('/administracao',    'HomeController@index')->name('administracao');
+        Route::get('/staff',            'HomeController@index')->name('staff');
+        Route::get('/elenco',           'HomeController@index')->name('elenco');
     });
 
     Route::name('equipes.')->prefix('/equipes')->group(function () {
-        Route::get('/', 'EquipeController@index')->name('listar');
-        Route::get('/inserir', 'EquipeController@edit')->name('inserir');
-        Route::get('/{id}', 'EquipeController@edit')->name('editar');
-        Route::delete('/{id}', 'EquipeController@delete')->name('excluir');
+        Route::get('/',         'EquipeController@index')->name('listar');
+        Route::get('/inserir',  'EquipeController@create')->name('inserir');
+        Route::post('/',        'EquipeController@store')->name('salvar');
+        Route::get('/{id}',     'EquipeController@edit')->name('editar');
+        Route::delete('/{id}',  'EquipeController@delete')->name('excluir');
     });
 });
