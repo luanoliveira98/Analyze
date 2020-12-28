8964ce464f840db978ca9b23eb07c2b65e19212e
Luan Oliveira <luanoliveira17@hotmail.com>
2020-12-28 02:00:52 -0300

    Criando component de form base

---
+++ app/View/Components/Forms/Base.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\View\Components\Forms;
+
+use Illuminate\View\Component;
+
+class Base extends Component
+{
+    /**
+     * Ação do formulário (Rota)
+     * 
+     * @var string
+     */
+    public $action;
+
+    /**
+     * Método do formulário
+     * 
+     * @var string
+     */
+    public $method;
+
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct($action, $method = 'GET')
+    {
+        $this->action = $action;
+        $this->method = $method;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|string
+     */
+    public function render()
+    {
+        return view('components.forms.base');
+    }
+}
