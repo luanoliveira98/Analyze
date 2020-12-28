876f0da90e4514324c44827ab16a8d1d4a53d6a2
Luan Oliveira <luanoliveira17@hotmail.com>
2020-12-28 12:04:05 -0300

    Atualizando input para suportar config

--- resources/views/components/forms/input.blade.php
+++ resources/views/components/forms/input.blade.php
@@ -2,7 +2,7 @@
 <div class="form-group">
     <label for="{{$name}}">{{$label}}</label>
     <input type="text" class="form-control @if($errors->has($name)) is-invalid @endif" id="{{$name}}" name="{{$name}}" placeholder="{{$placeholder}}" 
-        value="@if(old($name)) {{old($name)}} @else {{$value}} @endif">
+    @if(old($name)) value="{{old($name)}}" @else value="{{$value}}" @endif>
     <x-texts.small-error name="{{$name}}" />
 </div>
 @endif
