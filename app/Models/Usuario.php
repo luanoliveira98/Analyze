ba863ab216622a7c29043763981cf8116d43b14a
Luan Oliveira <luanoliveira17@hotmail.com>
2021-01-03 18:41:47 -0300

    Atualizando scope, criando hasmany e atualizando rules

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -9,9 +9,42 @@ class Usuario extends Model
 {
     use softDeletes;
     protected $table = 'usuarios';
+    protected $fillable = ['nome', 'email', 'nick', 'senha'];
 
-    public function scopeAdministracao($query)
+    public function scopeDiretoria($query)
     {
-        return $query->where('nivel', 'administracao');
+        return $query->whereHas('tags', function($q){
+            $q->where('nivel', 'Diretoria');
+        });
+    }
+
+    public function tags()
+    {
+        return $this->hasMany(UsuarioTag::class);
+    }
+
+    /**
+     * Cria array de regras para validação
+     * 
+     * @param   string      $action         Ação da rota
+     * @param   object      $request        Dados da requisição
+     * 
+     * @return  array
+     */
+    public static function getRules(string $action, object $request): array
+    {
+        $data = [
+            'nome'  => 'required',
+        ];
+
+        if($action == 'store') {
+            $data['senha'] = 'required|same:confirmar_senha';
+            $data['email'] = 'required|email|unique:usuarios,email,NULL,id,deleted_at,NULL';
+        } else if ($action == 'update') {
+            $data['senha'] = 'same:confirmar_senha';
+            $data['email'] = 'required|email|unique:usuarios,email,'.$request->id.',id,deleted_at,NULL';
+        }
+
+        return $data;
     }
 }
