a8dafbd71645cdb0e6e70e6447e275724cd3c81d
Luan Oliveira <luanoliveira17@hotmail.com>
2021-01-03 18:40:28 -0300

    Colocando nullable na coluna de nick de usuario

--- database/migrations/2020_12_28_215023_add_col_nick_to_usuarios_table.php
+++ database/migrations/2020_12_28_215023_add_col_nick_to_usuarios_table.php
@@ -14,7 +14,7 @@ class AddColNickToUsuariosTable extends Migration
     public function up()
     {
         Schema::table('usuarios', function (Blueprint $table) {
-            $table->string('nick')->after('email');
+            $table->string('nick')->nullable()->after('email');
         });
     }
 
